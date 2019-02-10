ce6601f0219b02c42a05c834188015355ef45f62
Hamad Almogbl <hamad.almogbl@gmail.com>
2019-02-10 17:22:18 +0300

    task_3 completed

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,6 +7,23 @@ def restaurant_list(request):
 
     context = {
 
+    	"my_list":[
+			{
+				
+				"restaurant_name":"Alderah",
+				"food_type":"Saudi Kabsa"
+			},
+			{
+				
+				"restaurant_name":"Italin Hot Hot",
+				"food_type":"Spicy Pizza"
+			},
+			{
+				
+				"restaurant_name":"Indian Pot",
+				"food_type":"Indian Food"
+			},
+	    ],
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +31,12 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+
+    		"my_object":
+    			{
+    				"restaurant_name":"Chipotle",
+    				"food_type":"Mexican American Food"
+    			}
 
     }
     return render(request, 'detail.html', context)
